98eb1e69d3a5947e499f5edc164b467501a4ff49
Pablo <ezekielgonzalezr@gmail.com>
2023-07-10 21:26:59 -0600

    Filter table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+
+SELECT name from animals WHERE date_of_birth BETWEEN '01-01-2016' AND '01-01-2019';
+
+SELECT name from animals WHERE (neutered = 't') AND ( escape_attempts < 3);
+
+SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+
+SELECT * from animals WHERE neutered = 't';
+
+SELECT * from animals WHERE NOT name = 'Gabumon';
+
+SELECT * from animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
