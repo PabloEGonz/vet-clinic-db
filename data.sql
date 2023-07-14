faaa3a390ce235015ee41039c9dfbf2f4b0307ac
Pablo <ezekielgonzalezr@gmail.com>
2023-07-13 21:58:47 -0600

    Insert new data

--- data.sql
+++ data.sql
@@ -8,34 +8,64 @@
 
 /*  Vet clinic database: query and update animals table */
 
-INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) 
-VALUES ('Charmander', 'Feb 8, 2020', 0, false, -11),
-('Plantmon', 'Nov 15, 2021', 2, true, -5.7),
-('Squirtle', 'Apr 2, 1993', 3, false, -12.13),
-('Angemon', 'Jun 12, 2005', 1, true, -45),
-('Boarmon', 'Jun 7, 2005', 7, true, 20.4),
-('Blossom', 'Oct 13, 1998', 3, true, 17),
-('Ditto', 'May 14, 2022', 4, true, 22);
+-- INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) 
+-- VALUES ('Charmander', 'Feb 8, 2020', 0, false, -11),
+-- ('Plantmon', 'Nov 15, 2021', 2, true, -5.7),
+-- ('Squirtle', 'Apr 2, 1993', 3, false, -12.13),
+-- ('Angemon', 'Jun 12, 2005', 1, true, -45),
+-- ('Boarmon', 'Jun 7, 2005', 7, true, 20.4),
+-- ('Blossom', 'Oct 13, 1998', 3, true, 17),
+-- ('Ditto', 'May 14, 2022', 4, true, 22);
 
-INSERT INTO owners (full_name, age)
-VALUES ('Sam Smith', 34),
-('Jennifer Orwell', 19),
-('Bob', 45),
-('Melody Pond', 77),
-('Dean Winchester', 14),
-('Jodie Whittaker ', 38);
+-- INSERT INTO owners (full_name, age)
+-- VALUES ('Sam Smith', 34),
+-- ('Jennifer Orwell', 19),
+-- ('Bob', 45),
+-- ('Melody Pond', 77),
+-- ('Dean Winchester', 14),
+-- ('Jodie Whittaker ', 38);
 
-INSERT INTO species (name)
-VALUES('Pokemon'), ('Digimon');
+-- INSERT INTO species (name)
+-- VALUES('Pokemon'), ('Digimon');
 
-UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
-UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+-- UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+-- UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
 
-UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
-UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
-UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
-UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+-- UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+-- UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+-- UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+-- UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+-- UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
 
+/*Vet clinic database: add "join table" for visits */
 
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES('William Tatcher', 45, 'Apr 23, 2000'),
+('Maisy Smith', 26, 'Jan 17, 2019'),
+('Stephanie Mendez', 64, 'May 4, 1981'),
+(' Jack Harkness', 38, 'Jun 8, 2008');
 
+INSERT INTO specializations (species_id, vets_id)
+VALUES (1, 1),(1,3),(2,3),(2,4);
+
+INSERT INTO visits(animal_id, vets_id, visit_date)
+VALUES(1, 1, 'May 24, 2020'),
+(1, 3, 'Jul 22, 2020'),
+(2, 4, 'Feb 2, 2021'),
+(5, 2, ' Jan 5, 2020'),
+(5, 2, 'Mar 8, 2020'),
+(5, 2, 'May 14, 2020'),
+(3, 3, 'May 4, 2021'),
+(9, 4, 'Feb 24, 2021'),
+(7, 2, 'Dec 21, 2019'),
+(7, 1, 'Aug 10, 2020'),
+(7, 2, 'Apr 7, 2021'),
+(10, 3, 'Sep 29, 2019'),
+(8, 4, 'Oct 3, 2020'),
+(8, 4, 'Nov 4, 2020'),
+(4, 2, 'Jan 24, 2019'),
+(4, 2, 'May 15, 2019'),
+(4, 2, 'Feb 27, 2020'),
+(4, 2, 'Aug 3, 2020'),
+(6, 3, 'May 24, 2020'),
+(6, 1, 'Jan 11, 2021');
