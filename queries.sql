56a93a94d70d07dbcfccc1ea20f86b38b621364e
Pablo <ezekielgonzalezr@gmail.com>
2023-07-17 15:13:22 -0600

    Create index for owners emails

--- queries.sql
+++ queries.sql
@@ -138,6 +138,10 @@ LIMIT 1;
 
 /*Used explain analyze to check the database performance*/
 
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+
 EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
